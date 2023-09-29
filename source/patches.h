4c2aadd45d9c6c4b34efd27920cd248f8eddeb64
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2023-09-28 23:59:52 -0400

    Add files via upload

--- source/patches.h
+++ source/patches.h
@@ -8,8 +8,11 @@ typedef struct URL_Patch
 } URL_Patch;
 
 static  const URL_Patch url_patches[] = {
+	//nim-boss .rodata
+	{0xE2299990, "nppl.nonamegiven.xyz"}, // I would love for this to work, but must curb my expectations.
+	
     //nim-boss .bss
-    {0xE24B8A24, "https://nppl.app.pretendo.cc/p01/policylist/1/1/UNK"}
+    {0xE24B8A24, "https://nppl.nonamegiven.xyz/p01/policylist/1/1/UNK"}
 };
 
 #endif
